17ec9689d49e0d4a99e5a3bd28908166591a3bcf
Anthony Barbier <anthony.barbier@arm.com>
2019-03-05 13:36:38 +0000

    Backed up awesome's config

---
+++ apps.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+sleep 1
+
+#gnome-screensaver &
+xscreensaver&
+nm-applet &
+gnome-session --session=awesome &
+#gnome-settings-daemon &
+#gnome-power-manager &
+#gnome-volume-control-applet &
+#alsa-tray &
+#checkgmail &
+#amsn &
+#pidgin&
+#tomboy &
+#davmail &
+skypeforlinux &
+thunderbird &
+google-chrome &
+#volti &
+pasystray &
+blueman-applet &
+#remmina &
+mount_eis.sh 
+mount_projects.sh 
+mount_arm.sh 
+mount_oldarm.sh 
+monitor-notifications.sh &
+
+sleep 2
+
+conky &
+pkill -9 indicator-appli
