42121eab63745fae0fcf77c5bcd348e78f47957e
Anthony Barbier <anthonyb@graphcore.ai>
2019-08-16 01:46:07 +0100

    Added hint about lid close

--- i3/install.sh
+++ i3/install.sh
@@ -16,6 +16,8 @@ EndSection
 EOF
 cp config ~/.config/i3/config 
 
+# https://github.com/ruudud/i3wm-scripts --> lock on lid close
+
 
 #Not needed: same version as already in Ubuntu
 #/usr/lib/apt/apt-helper download-file http://debian.sur5r.net/i3/pool/main/s/sur5r-keyring/sur5r-keyring_2019.02.01_all.deb keyring.deb SHA256:176af52de1a976f103f9809920d80d02411ac5e763f695327de9fa6aff23f416
