852d48c6ef36ab917db270a8dde6077561c0c0b6
Anthony Barbier <anthonyb@graphcore.ai>
2019-08-17 22:09:58 +0100

    Added power manager

--- i3/install.sh
+++ i3/install.sh
@@ -1,6 +1,8 @@
 sudo apt update
 sudo apt install -y i3 i3lock i3blocks i3status suckless-tools
-sudo apt install pavucontrol pasystray paprefs nm-applet
+sudo apt install pavucontrol pasystray paprefs nm-applet xfce4-power-manager
+xfce4-power-manager-settings # Enable systray icon!
+
 # Start nm-applet / pasystray
 # Fix touchpad: https://cravencode.com/post/essentials/enable-tap-to-click-in-i3wm/
 sudo mkdir -p /etc/X11/xorg.conf.d && sudo tee <<'EOF' /etc/X11/xorg.conf.d/90-touchpad.conf 1> /dev/null
