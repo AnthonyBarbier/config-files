7a5e2f18f80cbebeaa34452fac001a1d119e5357
Anthony Barbier <anthonyb@graphcore.ai>
2019-08-19 18:27:24 +0100

    Added support for extra workspaces

--- i3/bin/rename_workspace.sh
+++ i3/bin/rename_workspace.sh
@@ -2,10 +2,15 @@ set -e
 echo "New name=$1" >> ~/out.log
 if [ "$#" -lt 3 ]; then
   NUM=`i3-msg -t get_workspaces | jq '.[] | select(.focused==true).num'`
+  NUM_ID=$NUM;
+  if [ "$NUM" -gt 10 ]; then
+    NUM_ID=$((${NUM} -10))
+    NUM_ID=`echo $NUM_ID | tr '0-7' '0A-G'`
+  fi
 else
   NUM=$2
 fi
-NAME="\"$NUM: $1\""
+NAME="\"$NUM_ID: $1\""
 set_workspace_name.sh $NUM "$NAME"
 
 # Only do the actual renaming if the number wasn't set:
