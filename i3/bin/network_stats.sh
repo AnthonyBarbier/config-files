819f2b6356f2860ccffda6338138f44455159530
Anthony Barbier <anthonyb@graphcore.ai>
2019-08-27 17:47:17 +0100

    Updated network_stats.sh

--- i3/bin/network_stats.sh
+++ i3/bin/network_stats.sh
@@ -2,13 +2,13 @@
 
 #ip -h -j -s link
 
-out=""
 ip -h -j -s link | jq -c '.[] | select(.ifname != "lo") | select(.stats64.tx.bytes != 0) | select(.stats64.rx.bytes != 0) |  [.ifname,  .stats64
 .rx.bytes, .stats64.tx.bytes]'| tr '[]' ',,' | while read line; do
-  interface=`echo $line | cut -d"," -f2`
+  interface=`echo $line | cut -d"," -f2 | tr -d '"'`
   received=`echo $line | cut -d"," -f3`
   sent=`echo $line | cut -d"," -f4`
   received=`numfmt --to=iec-i --suffix=B $received`
   sent=`numfmt --to=iec-i --suffix=B $sent`
-  printf "$interface: RX $received / TX $sent "
+  printf "| $interface ↓$received/↑$sent "
 done
+printf "|"
