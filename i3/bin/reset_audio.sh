5db32035225bfb37eaa0eb70785a40239a72da31
Anthony Barbier <anthonyb@graphcore.ai>
2019-09-24 09:18:06 +0100

    Updated audio config

--- i3/bin/reset_audio.sh
+++ i3/bin/reset_audio.sh
@@ -1,3 +1,30 @@
 #!/bin/bash
 
+set_a2dp() {
 index=`pacmd list-cards | grep bluez_card -B1 | grep index | awk '{print $2}'`;pacmd set-card-profile $index off; pacmd set-card-profile $index a2dp_sink
+}
+
+move_streams() {
+index=`pacmd list-cards | grep bluez_card -B1 | grep index | awk '{print $2}'`;
+pacmd set-default-sink $index
+for input in `pacmd list-sink-inputs | grep index | awk '{print $2}'`; do
+  pacmd move-sink-input $input $index
+done
+}
+
+set_a2dp
+
+bluetoothctl << EOF
+disconnect CC:98:8B:AF:27:35
+EOF
+sleep 5
+
+bluetoothctl << EOF
+connect CC:98:8B:AF:27:35
+EOF
+
+sleep 5
+
+set_a2dp
+
+move_streams
