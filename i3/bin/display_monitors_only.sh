246d28b0b90dec0c3ca523e82c58597a1d77a604
Anthony Barbier <anthonyb@graphcore.ai>
2020-08-06 11:33:41 +0100

    Backup scripts

---
+++ i3/bin/display_monitors_only.sh
@@ -0,0 +1,4 @@
+#xrandr --output DP-1.1 --primary --auto --right-of eDP-1-1 --output DP-1.3 --auto --right-of DP-1.1
+# xrandr --listactivemonitors
+xrandr --output DP-1.2 --primary --auto --output eDP-1-1 --off --output DP-1.3 --auto --right-of DP-1.2
+
---
+++ i3/bin/screenshot_area.sh
@@ -0,0 +1,1 @@
+gnome-screenshot -a -f ~/Pictures/screenshots/$(date +%d-%m-%Y_%H-%M-%S).png
