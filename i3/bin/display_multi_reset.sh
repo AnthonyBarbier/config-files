58f7d18a08bc1f6bc3b252fabf60fc100a148b48
Anthony Barbier <anthonyb@graphcore.ai>
2020-08-06 11:32:55 +0100

    Switch to tilix

--- i3/bin/display_multi_reset.sh
+++ i3/bin/display_multi_reset.sh
@@ -1,1 +1,4 @@
-xrandr --output DP-1.1 --primary --auto --right-of eDP-1-1 --output DP-1.3 --auto --right-of DP-1.1
+#xrandr --output DP-1.1 --primary --auto --right-of eDP-1-1 --output DP-1.3 --auto --right-of DP-1.1
+# xrandr --listactivemonitors
+xrandr --output DP-1.2 --primary --auto --right-of eDP-1-1 --output DP-1.3 --auto --right-of DP-1.2
+
--- i3/bin/display_reset.sh
+++ i3/bin/display_reset.sh
@@ -1,1 +1,1 @@
-xrandr --output eDP-1-1 --primary
+xrandr --output eDP-1-1 --primary --output DP-1.2 --off --output DP-1.3 --off
